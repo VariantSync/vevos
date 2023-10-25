2e6081afc67452435280eccf65856c39491bfaba
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-10-25 12:06:23 +0200

    tests: add empty tests

---
+++ src/test/java/org/variantsync/vevos/extraction/ExtractionTest.java
@@ -0,0 +1,59 @@
+package org.variantsync.vevos.extraction;
+
+import org.junit.jupiter.api.Test;
+
+public class ExtractionTest {
+        @Test
+        public void linePresenceCondition() {
+        }
+
+        @Test
+        public void lineFeatureMapping() {
+
+        }
+
+        @Test
+        public void lineCodeMatching() {
+
+        }
+
+        @Test
+        public void filePresenceConditions() {
+
+        }
+
+        @Test
+        public void fileFeatureMappings() {
+
+        }
+
+        @Test
+        public void fileCodeMatching() {
+
+        }
+
+        @Test
+        public void fileGroundTruth() {
+
+        }
+
+        @Test
+        public void fileFeatures() {
+
+        }
+
+        @Test
+        public void commitFeatures() {
+
+        }
+
+        @Test
+        public void commitGroundTruth() {
+
+        }
+
+        @Test
+        public void commitChangedFiles() {
+
+    }
+}
